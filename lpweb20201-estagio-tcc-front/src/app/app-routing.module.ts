f4bedcf00419b75d3b0f25a8b9e797c25cf4af59
Jackson Gomes <jgomes@ceulp.edu.br>
2020-06-04 22:14:27 -0300

    feat(frontend): atualiza as rotas

--- lpweb20201-estagio-tcc-front/src/app/app-routing.module.ts
+++ lpweb20201-estagio-tcc-front/src/app/app-routing.module.ts
@@ -4,12 +4,23 @@ import { LoginComponent } from './login/login.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HomeInicioComponent } from './home-inicio/home-inicio.component';
+import { PropostasDeTCCComponent } from './propostas-de-tcc/propostas-de-tcc.component';
+import { PropostaDeTCCComponent } from './proposta-de-tcc/proposta-de-tcc.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'perfil', component: PerfilComponent },
   { path: 'sobre', component: SobreComponent },
+  {
+    path: 'inicio', component: InicioComponent, children: [
+      { path: 'propostas-de-tcc', component: PropostasDeTCCComponent },
+      { path: 'propostas-de-tcc/:id', component: PropostaDeTCCComponent },
+      { path: '', component: HomeInicioComponent }
+    ]
+  },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: PaginaNaoEncontradaComponent }
 ];
