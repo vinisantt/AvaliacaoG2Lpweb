9683a0f7dd485299540f626579043b6a6894d969
Jackson Gomes <jgomes@ceulp.edu.br>
2020-06-04 22:11:32 -0300

    feat(backend): configura o django-admin

--- lpweb20201_estagio_tcc_back/backend/admin.py
+++ lpweb20201_estagio_tcc_back/backend/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Perfil)
+admin.site.register(Curso)
+admin.site.register(Aluno)
+admin.site.register(AlunoUsuario)
+admin.site.register(Matricula)
+admin.site.register(Funcionario)
+admin.site.register(FuncionarioUsuario)
+admin.site.register(Professor)
+admin.site.register(ColaboradorExterno)
+admin.site.register(ColaboradorExternoUsuario)
+admin.site.register(Orientacao)
+admin.site.register(PropostaDeEstagio)
+admin.site.register(PropostaDeTCC)
+admin.site.register(MembroDeBancaDeProposta)
+admin.site.register(AvaliacaoDeProposta)
